6a205803fa63164cd218bc6f93c9240e2ce98694
Jason209 <shenjie209@gmail.com>
2012-03-19 23:57:03 +0800

    3.20

--- scripts/index.js
+++ scripts/index.js
@@ -14,22 +14,32 @@
 	/* 邮箱提醒 */
 	var mailarray=["gmail.com","sina.com","163.com","taobao.com"];
 	var tip=$('#username').closest('form').find('.emailtip');
-	for(var i = 0, l = mailarray.length; i < l; i++){
+	var flag=true;
+	for(var i = 0, l = mailarray.length; i <= l; i++){
 		tip.append("<li></li>");
 	}
 	document.getElementById('username').oninput=function(){
-		var changetext=$(this).val();
-		tip.show();		
+		var changetext=$(this).val(),temp;
+		flag=false;
+		tip.show();	
 		tip.find('li').each(function(index){
 			if(index == 0)
 				$(this).html(changetext);
+			else if(changetext.indexOf('@') != -1){
+				temp=changetext.split('@')[0];
+				$(this).html(temp+"@"+mailarray[index-1]);
+			}
 			else{
-				$(this).html(changetext+"@"+mailarray[index]);
+				$(this).html(changetext+"@"+mailarray[index-1]);
 			}
 		});
-		console.log(123);
 	}
-	$('#username').blur(function(){
+	$('body').delegate('ul.emailtip li','click',function(){
+		var val=$(this).text();
+		$('#username').val(val);
+		tip.hide();
+	});
+	$('body').delegate('#username','blur',function(){
 		tip.hide();
 	});
 	$('figure.welcome ul.emailtip li').hover(function(){
